d530384b94a6ede1bd4a5ddd5914ce85f436ca95
coach-wang <51986254+coach-wang@users.noreply.github.com>
2020-03-30 15:31:27 +0800

    Create 输入读取.py

---
+++ 输入读取.py
@@ -0,0 +1,18 @@
+#输入单个数字：
+a = int(input().strip())
+#输入单个字符串并转化为list：
+b = list(input().strip())
+#输入多个list
+P = []
+for i in range(n):
+    P.append(list(map(int, input().strip().split())))
+#创建字典：
+dic = {}
+idea = list()
+if idea[i] not in dic:
+    dic[idea[i]].append(idea)
+#如果不确定输入的数量，使用：
+while True:
+    try:
+    except:
+        break
