e13c7e983815f1d269e0cffc8587e26140ae78da
coach-wang <51986254+coach-wang@users.noreply.github.com>
2020-04-08 15:06:40 +0800

    Update 输入读取.py

--- 输入读取.py
+++ 输入读取.py
@@ -11,6 +11,10 @@ dic = {}
 idea = list()
 if idea[i] not in dic:
     dic[idea[i]].append(idea)
+#读取字典：
+for t in dic:   
+    lis.append(dic[t])  #把字典中的list存到lis中，注意这里的用法
+final_lis = sorted(lis, key=lambda k:(-k[2], k[3]))   #对lis进行排序
 #如果不确定输入的数量，使用：
 while True:
     try:
